7508656e77a62e3464e3508650b8cb4c5972429f
Mathieu Mariot <math.mariot@gmail.com>
2021-04-26 15:44:26 +0200

    -> Add RecyclerView (only in xml) -> Add Toolbar with a simpel menu

--- app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
@@ -1,13 +1,20 @@
 package com.openclassrooms.realestatemanager;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textViewMain;
     private TextView textViewQuantity;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         this.textViewMain = findViewById(R.id.activity_main_activity_text_view_main); //Modify with good id for resolv initial bug
         this.textViewQuantity = findViewById(R.id.activity_main_activity_text_view_quantity);
-
+        this.setupToolbar();
         this.configureTextViewMain();
         this.configureTextViewQuantity();
     }
@@ -30,5 +37,18 @@ public class MainActivity extends AppCompatActivity {
         int quantity = Utils.convertDollarToEuro(100);
         this.textViewQuantity.setTextSize(20);
         this.textViewQuantity.setText(String.valueOf(quantity)); // Use String.valueOf("Item") For resolve initial bug
+    }
+
+    private void setupToolbar(){
+        mToolbar = findViewById(R.id.materialToolbar);
+        setSupportActionBar(mToolbar);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.property_menu, menu);
+        return true;
     }
 }
